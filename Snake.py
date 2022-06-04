094f59b06069a52e40f40352275eee57cf5c87b6
Jarvis Lam <102021121+JarvisLam-LemonCEO@users.noreply.github.com>
2022-06-04 17:27:20 +0800

    Add files via upload

--- Snake.py
+++ Snake.py
@@ -1,210 +1,261 @@
-#Snake Tutorial Python
- 
-import math
-import random
+
 import pygame
-import tkinter as tk
-from tkinter import messagebox
- 
-class cube(object):
-    rows = 20
-    w = 500
-    def __init__(self,start,dirnx=1,dirny=0,color=(255,0,0)):
-        self.pos = start
-        self.dirnx = 1
-        self.dirny = 0
-        self.color = color
- 
-       
-    def move(self, dirnx, dirny):
-        self.dirnx = dirnx
-        self.dirny = dirny
-        self.pos = (self.pos[0] + self.dirnx, self.pos[1] + self.dirny)
- 
-    def draw(self, surface, eyes=False):
-        dis = self.w // self.rows
-        i = self.pos[0]
-        j = self.pos[1]
- 
-        pygame.draw.rect(surface, self.color, (i*dis+1,j*dis+1, dis-2, dis-2))
-        if eyes:
-            centre = dis//2
-            radius = 3
-            circleMiddle = (i*dis+centre-radius,j*dis+8)
-            circleMiddle2 = (i*dis + dis -radius*2, j*dis+8)
-            pygame.draw.circle(surface, (0,0,0), circleMiddle, radius)
-            pygame.draw.circle(surface, (0,0,0), circleMiddle2, radius)
-       
- 
- 
- 
-class snake(object):
-    body = []
-    turns = {}
-    def __init__(self, color, pos):
-        self.color = color
-        self.head = cube(pos)
-        self.body.append(self.head)
-        self.dirnx = 0
-        self.dirny = 1
- 
-    def move(self):
+import sys
+import copy
+import random
+import time
+
+pygame.init()
+
+width = 500
+height = 500
+scale = 10
+score = 0
+
+food_x = 10
+food_y = 10
+
+display = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Snake Game")
+clock = pygame.time.Clock()
+
+background = (23, 32, 42)
+snake_colour = (236, 240, 241)
+food_colour = (148, 49, 38)
+snake_head = (247, 220, 111)
+
+
+# ----------- Snake Class ----------------
+# self.history[0][0] is the location of the head of the snake
+
+class Snake:
+    def __init__(self, x_start, y_start):
+        self.x = x_start
+        self.y = y_start
+        self.w = 10
+        self.h = 10
+        self.x_dir = 1
+        self.y_dir = 0
+        self.history = [[self.x, self.y]]
+        self.length = 1
+
+    def reset(self):
+        self.x = width/2-scale
+        self.y = height/2-scale
+        self.w = 10
+        self.h = 10
+        self.x_dir = 1
+        self.y_dir = 0
+        self.history = [[self.x, self.y]]
+        self.length = 1
+    
+    #function to show the body of snake
+    def show(self):
+        for i in range(self.length):
+            if not i == 0:
+                pygame.draw.rect(display, snake_colour, (self.history[i][0], self.history[i][1], self.w, self.h))
+            else:
+                pygame.draw.rect(display, snake_head, (self.history[i][0], self.history[i][1], self.w, self.h))
+
+
+    def check_eaten(self):
+        if abs(self.history[0][0] - food_x) < scale and abs(self.history[0][1] - food_y) < scale:
+            return True
+
+    def grow(self):
+        self.length += 1
+        self.history.append(self.history[self.length-2])
+
+    def death(self):
+        i = self.length - 1
+        while i > 0:
+            if abs(self.history[0][0] - self.history[i][0]) < self.w and abs(self.history[0][1] - self.history[i][1]) < self.h and self.length > 2:
+                return True
+            i -= 1
+
+    def update(self):
+        i = self.length - 1
+        while i > 0:
+            self.history[i] = copy.deepcopy(self.history[i-1])
+            i -= 1
+        self.history[0][0] += self.x_dir*scale
+        self.history[0][1] += self.y_dir*scale
+    
+    def autoplay(self):
+
+        if abs(food_x-self.history[0][0]) < 10 and abs(food_y-self.history[0][1]) < 10:
+            # if self.check_eaten():
+            #     food.new_location()
+            #     score += 1
+            #     self.grow()
+            print("")
+        elif abs(food_x-self.history[0][0]) < 10:
+            
+            # if self.y_dir==1 or self.y_dir==-1:
+            #     self.y_dir=0
+            #     self.x_dir=1
+            if self.x_dir==1 or self.x_dir==-1:
+                if food_y>self.history[0][1]:
+                    self.y_dir=1
+                else:
+                    self.y_dir=-1
+                self.x_dir=0
+        elif abs(food_y-self.history[0][1]) < 10 :
+            
+            # if self.x_dir==1 or self.x_dir==-1:
+            #     self.x_dir=0
+            #     self.y_dir=1
+            if self.y_dir==1 or self.y_dir==-1:
+                self.y_dir=0
+                if food_x>self.history[0][0]:
+                    self.x_dir=1
+                else:
+                    self.x_dir=-1
+
+
+        elif food_x-self.history[0][0] >= 10  and food_y-self.history[0][1] >= 10:
+            
+            if self.x_dir==-1:
+                self.y_dir=1
+                self.x_dir=0
+            elif self.y_dir==-1:
+                self.y_dir=0
+                self.x_dir=1
+        elif self.history[0][0]-food_x >= 10  and food_y-self.history[0][1] >= 10:
+            
+            if self.x_dir==1:
+                self.y_dir=1
+                self.x_dir=0
+            elif self.y_dir==1:
+                self.y_dir=0
+                self.x_dir=-1
+        elif self.history[0][0]-food_x >= 10  and self.history[0][1]-food_y >= 10:
+            
+            if self.x_dir==1:
+                self.y_dir=-1
+                self.x_dir=0
+            elif self.y_dir==1:
+                self.y_dir=0
+                self.x_dir=-1
+
+        elif food_x-self.history[0][0] >= 10  and self.history[0][1]-food_y >= 10:
+            
+            if self.x_dir==-1:
+                self.y_dir=-1
+                self.x_dir=0
+            elif self.y_dir==1:
+                self.y_dir=0
+                self.x_dir=1
+        
+        self.update()
+        
+        
+        
+
+        
+
+
+
+# ----------- Food Class --------------
+class Food:
+    def new_location(self):
+        global food_x, food_y
+        food_x = random.randrange(1, width/scale-1)*scale
+        food_y = random.randrange(1, height/scale-1)*scale
+
+    def show(self):
+        pygame.draw.rect(display, food_colour, (food_x, food_y, scale, scale))
+
+
+def show_score():
+    font = pygame.font.SysFont("Copperplate Gothic Bold", 20)
+    text = font.render("Score: " + str(score), True, snake_colour)
+    display.blit(text, (scale, scale))
+
+
+# ----------- Main Game Loop -------------
+def gameLoop():
+    loop = True
+
+    global score
+
+    snake = Snake(width/2, height/2) #starting from mid of grid
+    food = Food()
+    food.new_location()
+    ap=False
+
+    while loop:
+
+        display.fill(background)
+        snake.show()
+        food.show()
+        show_score()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
- 
-            keys = pygame.key.get_pressed()
- 
-            for key in keys:
-                if keys[pygame.K_LEFT]:
-                    self.dirnx = -1
-                    self.dirny = 0
-                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
- 
-                elif keys[pygame.K_RIGHT]:
-                    self.dirnx = 1
-                    self.dirny = 0
-                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
- 
-                elif keys[pygame.K_UP]:
-                    self.dirnx = 0
-                    self.dirny = -1
-                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
- 
-                elif keys[pygame.K_DOWN]:
-                    self.dirnx = 0
-                    self.dirny = 1
-                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
- 
-        for i, c in enumerate(self.body):
-            p = c.pos[:]
-            if p in self.turns:
-                turn = self.turns[p]
-                c.move(turn[0],turn[1])
-                if i == len(self.body)-1:
-                    self.turns.pop(p)
-            else:
-                if c.dirnx == -1 and c.pos[0] <= 0: c.pos = (c.rows-1, c.pos[1])
-                elif c.dirnx == 1 and c.pos[0] >= c.rows-1: c.pos = (0,c.pos[1])
-                elif c.dirny == 1 and c.pos[1] >= c.rows-1: c.pos = (c.pos[0], 0)
-                elif c.dirny == -1 and c.pos[1] <= 0: c.pos = (c.pos[0],c.rows-1)
-                else: c.move(c.dirnx,c.dirny)
-       
- 
-    def reset(self, pos):
-        self.head = cube(pos)
-        self.body = []
-        self.body.append(self.head)
-        self.turns = {}
-        self.dirnx = 0
-        self.dirny = 1
- 
- 
-    def addCube(self):
-        tail = self.body[-1]
-        dx, dy = tail.dirnx, tail.dirny
- 
-        if dx == 1 and dy == 0:
-            self.body.append(cube((tail.pos[0]-1,tail.pos[1])))
-        elif dx == -1 and dy == 0:
-            self.body.append(cube((tail.pos[0]+1,tail.pos[1])))
-        elif dx == 0 and dy == 1:
-            self.body.append(cube((tail.pos[0],tail.pos[1]-1)))
-        elif dx == 0 and dy == -1:
-            self.body.append(cube((tail.pos[0],tail.pos[1]+1)))
- 
-        self.body[-1].dirnx = dx
-        self.body[-1].dirny = dy
-       
- 
-    def draw(self, surface):
-        for i, c in enumerate(self.body):
-            if i ==0:
-                c.draw(surface, True)
-            else:
-                c.draw(surface)
- 
- 
-def drawGrid(w, rows, surface):
-    sizeBtwn = w // rows
- 
-    x = 0
-    y = 0
-    for l in range(rows):
-        x = x + sizeBtwn
-        y = y + sizeBtwn
- 
-        pygame.draw.line(surface, (255,255,255), (x,0),(x,w))
-        pygame.draw.line(surface, (255,255,255), (0,y),(w,y))
-       
- 
-def redrawWindow(surface):
-    global rows, width, s, snack
-    surface.fill((0,0,0))
-    s.draw(surface)
-    snack.draw(surface)
-    drawGrid(width,rows, surface)
-    pygame.display.update()
- 
- 
-def randomSnack(rows, item):
- 
-    positions = item.body
- 
-    while True:
-        x = random.randrange(rows)
-        y = random.randrange(rows)
-        if len(list(filter(lambda z:z.pos == (x,y), positions))) > 0:
-            continue
+                sys.exit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    pygame.quit()
+                    sys.exit()
+                if event.key==pygame.K_SPACE: #autoplay start
+                    ap=True
+                if event.key==pygame.K_TAB: #autoplay end
+                    ap=False
+                else:
+                    if snake.y_dir == 0:
+                        if event.key == pygame.K_UP:
+                            snake.x_dir = 0
+                            snake.y_dir = -1
+                        if event.key == pygame.K_DOWN:
+                            snake.x_dir = 0
+                            snake.y_dir = 1
+
+                    if snake.x_dir == 0:
+                        if event.key == pygame.K_LEFT:
+                            snake.x_dir = -1
+                            snake.y_dir = 0
+                        if event.key == pygame.K_RIGHT:
+                            snake.x_dir = 1
+                            snake.y_dir = 0
+
+        
+        if ap:
+            snake.autoplay()
         else:
-            break
-       
-    return (x,y)
- 
- 
-def message_box(subject, content):
-    root = tk.Tk()
-    root.attributes("-topmost", True)
-    root.withdraw()
-    messagebox.showinfo(subject, content)
-    try:
-        root.destroy()
-    except:
-        pass
- 
- 
-def main():
-    global width, rows, s, snack
-    width = 500
-    rows = 20
-    win = pygame.display.set_mode((width, width))
-    s = snake((255,0,0), (10,10))
-    snack = cube(randomSnack(rows, s), color=(0,255,0))
-    flag = True
- 
-    clock = pygame.time.Clock()
-   
-    while flag:
-        pygame.time.delay(50)
-        clock.tick(10)
-        s.move()
-        if s.body[0].pos == snack.pos:
-            s.addCube()
-            snack = cube(randomSnack(rows, s), color=(0,255,0))
- 
-        for x in range(len(s.body)):
-            if s.body[x].pos in list(map(lambda z:z.pos,s.body[x+1:])):
-                print('Score: ', len(s.body))
-                message_box('You Lost!')
-                s.reset((10,10))
-                break
- 
-           
-        redrawWindow(win)
- 
-       
-    pass
- 
- 
- 
-main()
+            snake.update()
+        
+
+        if snake.check_eaten():
+            food.new_location()
+            score += 1
+            snake.grow()
+
+
+        if snake.death():
+            score = 0
+            font = pygame.font.SysFont("Copperplate Gothic Bold", 50)
+            text = font.render("Game Over!", True, snake_colour)
+            display.blit(text, (width/2-50, height/2))
+            pygame.display.update()
+            time.sleep(3)
+            snake.reset()
+
+        
+        #updating the values if snake goes out of board
+        if snake.history[0][0] > width:
+            snake.history[0][0] = 0
+        if snake.history[0][0] < 0:
+            snake.history[0][0] = width
+
+        if snake.history[0][1] > height:
+            snake.history[0][1] = 0
+        if snake.history[0][1] < 0:
+            snake.history[0][1] = height
+
+        pygame.display.update()
+        clock.tick(10) #at most 10 frames should pass in 1 sec, it is used to control the speed of snake 
+
+gameLoop()
